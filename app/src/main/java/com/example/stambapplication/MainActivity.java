ff6315596f95abea2b0ee31b2f5758fa019d1c25
mustapha chatbi <chatbimdev@gmail.com>
2021-11-20 14:08:26 +0100

    project init

---
+++ app/src/main/java/com/example/stambapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.stambapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+
+    }
+
+
+}
