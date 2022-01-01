a963ec6f0a38cb5786dd3afc7ae8b0837542d56d
mustapha chatbi <chatbimdev@gmail.com>
2022-01-01 14:56:45 +0100

    update class

--- app/src/main/java/com/example/stambapplication/ClassAdapter.java
+++ app/src/main/java/com/example/stambapplication/ClassAdapter.java
@@ -3,13 +3,16 @@ package com.example.stambapplication;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.Image;
+import android.text.Editable;
 import android.text.Layout;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -23,12 +26,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,23 +52,21 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
     private List<ClassModel> classList;
     private OnDeleteClassListener deleteClassListener;
     private OnEditClassListener editClassListener;
-    private OnPopupConfirmClickListener onPopupConfirmClickLister;
-    private OnPopupCancelCancelListener onPopupCancelCancelListener;
     private boolean isArchived = false;
+
+    private String updatedModule;
+    private String updatedSpecialty;
+    private String updatedGrade;
 
 
     public ClassAdapter(List<ClassModel> classList, OnClassClickListener clickListener,
                         OnDeleteClassListener deleteClassListener,
                         OnEditClassListener editClassListener,
-                        OnPopupConfirmClickListener onPopupConfirmClickListener,
-                        OnPopupCancelCancelListener onPopupCancelCancelListener,
                         boolean isArchived) {
         this.classList = classList;
         this.clickListener = clickListener;
         this.deleteClassListener = deleteClassListener;
         this.editClassListener = editClassListener;
-        this.onPopupConfirmClickLister = onPopupConfirmClickListener;
-        this.onPopupCancelCancelListener = onPopupCancelCancelListener;
         this.isArchived = isArchived;
     }
 
@@ -85,7 +92,7 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ClassAdapter.ViewHolder holder, int position) {
-        holder.bind(classList.get(position), clickListener, deleteClassListener, editClassListener, onPopupConfirmClickLister, onPopupCancelCancelListener);
+        holder.bind(classList.get(position), clickListener, deleteClassListener, editClassListener);
 
         ClassModel classModel = classList.get(position);
 
@@ -115,6 +122,15 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
         notifyItemRemoved(position);
     }
 
+    public void updateClass(ClassModel classModel){
+        int position = classList.indexOf(classModel);
+        classList.get(position).setGrade(Integer.parseInt(updatedGrade));
+        classList.get(position).setModule(updatedModule);
+        classList.get(position).setSpecialty(updatedSpecialty);
+
+        notifyItemChanged(position);
+    }
+
     public void deleteClassRequest(ClassModel classModel) {
         int classId = classModel.getId();
         StringRequest deleteClassRequest = new StringRequest(Request.Method.DELETE, url + "class/" + classId, new Response.Listener<String>() {
@@ -127,7 +143,6 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
                     showToast(true, "Deleted Class Successfully");
                     removeClass(classModel);
                 } catch (Exception e) {
-                    System.out.println("errooor");
                     showToast(true, "Class was not deleted , please try again");
                     e.printStackTrace();
                 }
@@ -142,6 +157,50 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
         );
 
         RequestQueueSingleton.getInstance(appContext).addToRequestQueue(deleteClassRequest);
+    }
+
+    public void updateClassRequest(ClassModel classModel){
+        int classId = classModel.getId();
+
+        JSONObject jsonBody = new JSONObject();
+
+        try {
+            jsonBody.put("id", classId);
+            jsonBody.put("module", updatedModule);
+            jsonBody.put("specialty", updatedSpecialty);
+            jsonBody.put("grade", updatedGrade);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest updateClassRequest = new JsonObjectRequest(Request.Method.PUT, url + "class/" + classId, jsonBody, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    ClassModel updatedClassModel = objectMapper.readValue(response.toString(), new TypeReference<ClassModel>() {
+                    });
+                    Log.d("updated class ", response.toString());
+                    updateClass(classModel);
+                } catch (JsonParseException e) {
+                    e.printStackTrace();
+                } catch (JsonMappingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                showToast(true,"Class was updated !");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Unable to update a class", error.toString());
+                showToast(false,"Unable to update a class !");
+            }
+        });
+
+        RequestQueueSingleton.getInstance(appContext).addToRequestQueue(updateClassRequest);
+
     }
 
     public void showToast(boolean isSuccessful, String message) {
@@ -164,15 +223,6 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
         void onClassEditClick(PopupWindow popupWindow, ClassModel classModel);
     }
 
-
-    public interface OnPopupConfirmClickListener {
-        void onPopupConfirmClickListener(ClassModel classModel);
-    }
-
-    public interface OnPopupCancelCancelListener {
-        void onPopupCancelClickListener(ClassModel classModel);
-    }
-
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView classItemText;
         public ImageView deleteClassIcon;
@@ -191,9 +241,7 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
         public void bind(ClassModel item,
                          final OnClassClickListener onClassClicklistener,
                          final OnDeleteClassListener onClassDeleteClickListener,
-                         final OnEditClassListener onEditClassListener,
-                         final OnPopupConfirmClickListener onPopupConfirmClickListener,
-                         final OnPopupCancelCancelListener onPopupCancelCancelListener
+                         final OnEditClassListener onEditClassListener
         ) {
             classItemText.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -221,7 +269,6 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
                         @Override
                         public void onClick(View v) {
                             deleteClassRequest(item);
-                            onPopupConfirmClickListener.onPopupConfirmClickListener(item);
                             popupWindow.dismiss();
                         }
                     });
@@ -246,13 +293,93 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
                     int height = LinearLayout.LayoutParams.WRAP_CONTENT;
                     boolean focusable = true; // lets taps outside the popup also dismiss it
                     final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-                    onEditClassListener.onClassEditClick(popupWindow, item);
+
+                    onEditClassListener.onClassEditClick(popupWindow,item);
+
+                    Button cancelBtn = popupView.findViewById(R.id.cancelBtn);
+                    Button confirmBtn = popupView.findViewById(R.id.confirmBtn);
+
+                    EditText moduleInput = popupView.findViewById(R.id.editModuleInput);
+                    EditText specialtyInput = popupView.findViewById(R.id.editSpecialtyInput);
+                    EditText gradeInput = popupView.findViewById(R.id.editGradeInput);
+
+                    updatedGrade = gradeInput.getText().toString();
+                    updatedModule = moduleInput.getText().toString();
+                    updatedSpecialty = specialtyInput.getText().toString();
+
+
+                    moduleInput.addTextChangedListener(new TextWatcher() {
+                        public void afterTextChanged(Editable module) {
+                            updatedModule = module.toString();
+                            if (module.toString().length() > 0 && specialtyInput.getText().length() > 0 && gradeInput.getText().toString().trim().length() > 0) {
+                                confirmBtn.setEnabled(true);
+                            } else {
+                                confirmBtn.setEnabled(false);
+                            }
+                        }
+
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        }
+                    });
+
+                    specialtyInput.addTextChangedListener(new TextWatcher() {
+                        public void afterTextChanged(Editable specialty) {
+                            updatedSpecialty = specialty.toString();
+                            if (moduleInput.getText().length() > 0 && specialty.toString().length() > 0 && gradeInput.getText().toString().trim().length() > 0) {
+                                confirmBtn.setEnabled(true);
+                            } else {
+                                confirmBtn.setEnabled(false);
+                            }
+                        }
+
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        }
+                    });
+
+                    gradeInput.addTextChangedListener(new TextWatcher() {
+                        public void afterTextChanged(Editable grade) {
+                            updatedGrade = grade.toString();
+                            if (moduleInput.getText().length() > 0 && specialtyInput.getText().length() > 0 && grade.toString().trim().length() > 0) {
+                                confirmBtn.setEnabled(true);
+                            } else {
+                                confirmBtn.setEnabled(false);
+                            }
+                        }
+
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        }
+                    });
+
+                    confirmBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            updateClassRequest(item);
+                            popupWindow.dismiss();
+                        }
+                    });
+
+                    cancelBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            popupWindow.dismiss();
+                        }
+                    });
                 }
             });
 
         }
 
     }
+
 
 
 }
--- app/src/main/java/com/example/stambapplication/MainActivity.java
+++ app/src/main/java/com/example/stambapplication/MainActivity.java
@@ -4,59 +4,39 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.multidex.MultiDex;
-import androidx.multidex.MultiDexApplication;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     public static int CLASS_ID = 0;
@@ -65,11 +45,6 @@ public class MainActivity extends AppCompatActivity {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private RequestQueue queue;
     private ArrayList<ClassModel> activeClasses = new ArrayList<ClassModel>();
-    private ArrayList<ClassModel> archivedClasses = new ArrayList<ClassModel>();
-    private ClassAdapter activeClassListAdapter;
-
-    private ClassModel selectedClassModel;
-
     private final ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -82,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
-    private ClassAdapter archivedClassListAdapter;
+    private ArrayList<ClassModel> archivedClasses = new ArrayList<ClassModel>();
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -179,12 +154,34 @@ public class MainActivity extends AppCompatActivity {
         popupView.setBackgroundColor(Color.parseColor("#FFFFFF"));
     }
 
-    public  void populateDeletePopupContent(View popupView, ClassModel classModel) {
+    public void populateDeletePopupContent(View popupView, ClassModel classModel) {
         TextView popupTitle = popupView.findViewById(R.id.popupTitle);
         popupTitle.setText("Delete Class");
 
         TextView popupContent = popupView.findViewById(R.id.popupContent);
         popupContent.setText("Are you sure you want to delete class with name  " + classModel.getName());
+
+        findViewById(R.id.main).setBackgroundColor(Color.parseColor("#989696"));
+        popupView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+    }
+
+    public void populateEditPopupContent(View popupView, ClassModel classModel) {
+        TextView popupTitle = popupView.findViewById(R.id.popupTitle);
+        popupTitle.setText("Edit Class");
+
+        TextView popupContent = popupView.findViewById(R.id.popupContent);
+        popupContent.setText("Are you sure you want to delete class with name  " + classModel.getName());
+
+        popupView.findViewById(R.id.popupContent).setVisibility(View.GONE);
+
+        EditText moduleInput = popupView.findViewById(R.id.editModuleInput);
+        EditText specialtyInput = popupView.findViewById(R.id.editSpecialtyInput);
+        EditText gradeInput = popupView.findViewById(R.id.editGradeInput);
+
+        moduleInput.setText(classModel.getModule());
+        specialtyInput.setText(classModel.getSpecialty());
+        gradeInput.setText(String.valueOf(classModel.getGrade()));
+        gradeInput.setFilters(new InputFilter[]{new InputFilterMinMax("1", "5")});
 
         findViewById(R.id.main).setBackgroundColor(Color.parseColor("#989696"));
         popupView.setBackgroundColor(Color.parseColor("#FFFFFF"));
@@ -219,18 +216,34 @@ public class MainActivity extends AppCompatActivity {
             popupWindow.showAtLocation(popupWindow.getContentView(), Gravity.CENTER, 0, 0);
 
         }, (popupWindow, classModel) -> {
-        }, (classModel)-> {
-//            deleteClassRequest(classModel);
-        },classModel -> {
 
-        } ,false
+            View popupView = popupWindow.getContentView();
+
+            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    popupView.findViewById(R.id.main).setBackgroundColor(Color.parseColor("#FFFFFF"));
+                }
+            });
+            populateEditPopupContent(popupView, classModel);
+
+            popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    findViewById(R.id.main).setBackgroundColor(Color.parseColor("#FFFFFF"));
+                }
+            });
+
+            popupWindow.showAtLocation(popupWindow.getContentView(), Gravity.CENTER, 0, 0);
+
+        }, false
         );
 
         activeClassListView.setAdapter(activeClassListAdapter);
         activeClassListView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    public void createArchivedClasses(ArrayList<ClassModel> archivedClasses)  {
+    public void createArchivedClasses(ArrayList<ClassModel> archivedClasses) {
         RecyclerView archivedClassListView = findViewById(R.id.archivedClassListView);
 
         List<ClassModel> archivedClassList = archivedClasses;
@@ -242,41 +255,5 @@ public class MainActivity extends AppCompatActivity {
         archivedClassListView.setAdapter(archivedClassListAdapter);
         archivedClassListView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-//    public void deleteClassRequest(ClassModel classModel) {
-//        int classId = classModel.getId();
-//        StringRequest deleteClassRequest = new StringRequest(Request.Method.DELETE, url + "class/" + classId, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                try {
-//                    selectedClassModel = objectMapper.readValue(response, new TypeReference<ClassModel>() {
-//                    });
-//                    Log.d("deleted class ", response);
-//                }catch (Exception e){
-//                    System.out.println("errooor");
-//                    e.printStackTrace();
-//                }
-//                showToast(true, "Deleted Class Successfully");
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                try {
-//                    throw  new Exception(error.toString());
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                Log.d("delete class error", error.toString());
-//                showToast(true, "Class was not deleted , please try again");
-//            }
-//        }
-//        );
-//
-//        RequestQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(deleteClassRequest);
-//    }
-
-    public void updateClassRequest(ClassModel classModel) {
-    }
-
 
 }
