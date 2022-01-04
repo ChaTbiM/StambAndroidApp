91ecd8b42a40c8c1dd90a14bb6bc4dfd1ee4c017
mustapha chatbi <chatbimdev@gmail.com>
2022-01-04 07:32:45 +0100

    WIP import group from excel file

--- app/src/main/java/com/example/stambapplication/GroupsActivity.java
+++ app/src/main/java/com/example/stambapplication/GroupsActivity.java
@@ -1,16 +1,26 @@
 package com.example.stambapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,15 +37,61 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.extractor.ExcelExtractor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GroupsActivity extends AppCompatActivity {
     private final String url = "http://10.0.2.2:3000/";
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private File copiedGroupExcelFile;
+    private InputStream openedGroupInputStream;
 
     private List<GroupModel> groupList = new ArrayList<>();
+
+    private final ActivityResultLauncher<Intent> importGroupsLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+
+                        Uri fileUri = Uri.parse(intent.getDataString());
+
+
+                    }
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +123,17 @@ public class GroupsActivity extends AppCompatActivity {
                 Log.d("error", error.toString());
             }
         });
+
+
+        Button importGroupsBtn = findViewById(R.id.importGroupBtn);
+
+        importGroupsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchDataFromExcelFile();
+            }
+        });
+
 
 
         ActionBar actionBar = getSupportActionBar();
@@ -125,6 +192,119 @@ public class GroupsActivity extends AppCompatActivity {
         popupView.setBackgroundColor(Color.parseColor("#FFFFFF"));
     }
 
+
+    public void fetchDataFromExcelFile (){
+        Intent pickFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        pickFileIntent.setType("*/*");
+        pickFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        pickFileIntent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        importGroupsLauncher.launch(pickFileIntent);
+    }
+
+//    public void importFile(Uri uri) throws IOException {
+//        String fileName = getFileName(uri);
+//
+//        // The temp file could be whatever you want
+////        File fileCopy = copyToTempFile(uri, dynamic);
+//        File fileCopy = copyToTempFile(uri ,openedGroupExcelFile);
+//
+//
+//    }
+
+    public InputStream getDataFromExcel (Uri uri) {
+
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            return inputStream;
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Obtains the file name for a URI using content resolvers. Taken from the following link
+     * https://developer.android.com/training/secure-file-sharing/retrieve-info.html#RetrieveFileInfo
+     *
+     * @param uri a uri to query
+     * @return the file name with no path
+     * @throws IllegalArgumentException if the query is null, empty, or the column doesn't exist
+     */
+    private String getFileName(Uri uri) throws IllegalArgumentException {
+        // Obtain a cursor with information regarding this uri
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            throw new IllegalArgumentException("Can't obtain file name, cursor is empty");
+        }
+
+        cursor.moveToFirst();
+
+        String fileName = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
+
+        cursor.close();
+
+        return fileName;
+    }
+
+    /**
+     * Copies a uri reference to a temporary file
+     *
+     * @param uri      the uri used as the input stream
+     * @param tempFile the file used as an output stream
+     * @return the input tempFile for convenience
+     * @throws IOException if an error occurs
+     */
+    private File copyToTempFile(Uri uri, File tempFile) throws IOException {
+        // Obtain an input stream from the uri
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+
+        if (inputStream == null) {
+            throw new IOException("Unable to obtain input stream from URI");
+        }
+
+        // Copy the stream to the temp file
+        FileUtils.copyInputStreamToFile(inputStream, tempFile);
+
+        return tempFile;
+    }
+
+    private static String getCellAsString(Row row, int c, FormulaEvaluator formulaEvaluator) {
+        String value = "";
+        try {
+            Cell cell = row.getCell(c);
+            CellValue cellValue = formulaEvaluator.evaluate(cell);
+            switch (cellValue.getCellType()) {
+                case BOOLEAN:
+                    value = ""+cellValue.getBooleanValue();
+                    break;
+                case NUMERIC:
+                    double numericValue = cellValue.getNumberValue();
+                    if(DateUtil.isCellDateFormatted(cell)) {
+                        double date = cellValue.getNumberValue();
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+                        value = formatter.format(DateUtil.getJavaDate(date));
+                    } else {
+                        value = ""+numericValue;
+                    }
+                    break;
+                case STRING:
+                    value = ""+cellValue.getStringValue();
+                    break;
+                default:
+                    break;
+            }
+        } catch (NullPointerException e) {
+            /* proper error handling should be here */
+            Log.d("error output" ,e.toString());
+        }
+        return value;
+    }
 
 
 
